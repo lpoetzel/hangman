c8240bd06c7dc22f1195fe89f46e10cc2c7508a5
Lars Poetzel <lars.poetzel@gmail.com>
2023-03-08 09:51:27 +0100

    random wortToGuess

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+import words from "./wordList.json";
+
 function App() {
+  const [wordtoGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+  console.log(wordtoGuess);
   return <div>hi</div>;
 }
 

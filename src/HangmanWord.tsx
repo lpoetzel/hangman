f83f3f317b4e30451ab3a81677032236271e8509
Lars Poetzel <lars.poetzel@gmail.com>
2023-03-08 10:19:08 +0100

    HandmanWord

---
+++ src/HangmanWord.tsx
@@ -0,0 +1,30 @@
+export default function HangmanWord({}) {
+  const word = "test";
+  const guessedLetters = ["t", "e", "g"];
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: "0.25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? "visible"
+                : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
+}

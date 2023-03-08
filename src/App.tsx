e5eaff61c6dabd211b8d3c49cad37a459c459259
Lars Poetzel <lars.poetzel@gmail.com>
2023-03-08 10:33:49 +0100

    Keyboard setup and style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 import { useState } from "react";
+import HangmanDrawing from "./HangmanDrawing";
+import HangmanWord from "./HangmanWord";
+import Keyboard from "./Keyboard";
 import words from "./wordList.json";
 
 function App() {
   const [wordtoGuess, setWordToGuess] = useState(() => {
     return words[Math.floor(Math.random() * words.length)];
   });
-  console.log(wordtoGuess);
-  return <div>hi</div>;
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <div style={{ alignSelf: "stretch" }}>
+        <Keyboard />
+      </div>
+    </div>
+  );
 }
 
 export default App;
